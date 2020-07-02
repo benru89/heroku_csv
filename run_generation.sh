e7d6c6992d284825eb618317e5533edb1fd98b9f
Ruben <rubenfb14@gmail.com>
2020-07-03 00:37:06 +0200

    add req

--- run_generation.sh
+++ run_generation.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 wget -O inventario.csv https://www.depau.es/webservices/prestashop/968a9338-5e52-49b4-e895-a5779de8dacb/csv
+pip install pandas
+pip install pyyaml
+pip install unidecode
 python tratar_csv.py

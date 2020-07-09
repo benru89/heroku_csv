91d2cee861f58b38ff703db1d24ccddfa72f1fae
Ruben <rubenfb14@gmail.com>
2020-07-09 12:27:12 +0200

    save images.csv in cache dir

--- run_generation.sh
+++ run_generation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-wget -O inventario.csv https://www.depau.es/webservices/prestashop/968a9338-5e52-49b4-e895-a5779de8dacb/csv -q
-if [ -n "$1" ]
+wget -O inventario.csv https://www.depau.es/webservices/prestashop/968a9338-5e52-49b4-e895-a5779de8dacb/csv 
+if [ (-n "$1") || (-z $NETLIFY_CACHE_DIR/images.csv) ]
   then
-    wget -O images.csv https://www.depau.es/webservices/tarifa_personalizada/968a9338-5e52-49b4-e895-a5779de8dacb/csv -q
+    wget -O images.csv https://www.depau.es/webservices/tarifa_personalizada/968a9338-5e52-49b4-e895-a5779de8dacb/csv 
+    echo "copying images.csv to cache dir"
+    cp images.csv $NETLIFY_CACHE_DIR/images.csv
 fi
 python tratar_csv.py

b472cff49fe1bafe16dd3a28eabb47cd7521772c
Ruben <rubenfb14@gmail.com>
2020-07-09 13:07:44 +0200

    x

--- run_generation.sh
+++ run_generation.sh
@@ -4,6 +4,8 @@ if [ -n "$1" ] || [ ! -f "$NETLIFY_BUILD_BASE/.cache/images.csv" ]
   then
     wget -O images.csv https://www.depau.es/webservices/tarifa_personalizada/968a9338-5e52-49b4-e895-a5779de8dacb/csv 
     echo "copying images.csv to cache dir"
-    cp images.csv $NETLIFY_BUILD_BASE/.cache/images.csv
+    echo "$NETLIFY_BUILD_BASE"
+    echo "$NETLIFY_CACHE_DIR"
+    cp images.csv $NETLIFY_BUILD_BASE/cache/images.csv
 fi
 python tratar_csv.py

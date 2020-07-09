eb3faaef464ec3d522ef192790e56d7a93d73e92
Ruben <rubenfb14@gmail.com>
2020-07-09 11:51:39 +0200

    add images csv

--- run_generation.sh
+++ run_generation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -O inventario.csv https://www.depau.es/webservices/prestashop/968a9338-5e52-49b4-e895-a5779de8dacb/csv -q
-if [ -z "$1" ]
+if [ -n "$1" ]
   then
     wget -O images.csv https://www.depau.es/webservices/tarifa_personalizada/968a9338-5e52-49b4-e895-a5779de8dacb/csv -q
 fi
